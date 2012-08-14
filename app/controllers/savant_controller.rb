491fee681ebd467b08b566d5c82d6b1f2fa9e669
Kasper <kasper@kasper-ThinkPad-X121e.(none)>
2012-08-14 12:14:25 +0200

    google login

--- app/controllers/savant_controller.rb
+++ app/controllers/savant_controller.rb
@@ -1,13 +1,28 @@
 class SavantController < ApplicationController
 	def login
-
+		if session[:token]
+			render(:action => 'overview')
+		end
 	end
 
 	def do_login
 		@user = User.new(params[:user])
-		@user.valid?
-		
-		render(:action => 'login')
+		if @user.valid?
+			client_login_handler = GData::Auth::ClientLogin.new('writely', :account_type => 'HOSTED')
+			token = client_login_handler.get_token(@user.email, @user.password, 'savant')
+			client = GData::Client::Base.new(:auth_handler => client_login_handler)
+
+			session[:token] = token
+			session[:client] = client
+
+			render(:action => 'overview')
+		else
+			render(:action => 'login')
+		end
+	end
+
+	def overview
+
 	end
 
 	def all
